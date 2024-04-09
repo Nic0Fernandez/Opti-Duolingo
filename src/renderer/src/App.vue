22d8e6e4aacd4c6ba69540804b9f364130e1521a
Nicolas Fernandez <nicofer1404@gmail.com>
2024-04-09 20:53:23 +0200

    commit installation et fonctionnement turn.js et jquerry

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import './styles/base.css'
+import './styles/flipbook.css'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  $('#flipbook').turn({
+    width: 800,²
+    height: 600,
+    autoCenter: true
+  })
+})
+</script>
+
+<template>
+  <div id="flipbook">
+    <div class="hard">Turn.js</div>
+    <div class="hard"></div>
+    <div class="page">Page 1</div>
+    <div class="page">Page 2</div>
+    <div class="page">Page 3</div>
+    <div class="page">Page 4</div>
+    <div class="hard"></div>
+    <div class="hard"></div>
+  </div>
+  <Versions />
+</template>
