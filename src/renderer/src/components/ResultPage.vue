5e0f146d4c5ec327b5581cad002bab2ae2225eb1
Nicolas Fernandez <nicofer1404@gmail.com>
2024-04-22 13:49:48 +0200

    image result

--- src/renderer/src/components/ResultPage.vue
+++ src/renderer/src/components/ResultPage.vue
@@ -1,13 +1,13 @@
 <script setup>
 import '../styles/resultPage.css'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const props = defineProps({
-  score: String,
+  score: Number,
   onReturnHome: Function
 })
 
-let img
+let imgScore = ref('')
 
 onMounted(() => {
   displayImgResult()
@@ -18,21 +18,24 @@ function returnToHomePage() {
 }
 
 function displayImgResult() {
-  if (score<4) {
-    img = "https://raw.githubusercontent.com/Nic0Fernandez/Opti-Duolingo/main/src/renderer/src/assets/images/shakespeareProud.webp"
+  if (props.score < 4) {
+    imgScore.value =
+      'https://raw.githubusercontent.com/Nic0Fernandez/Opti-Duolingo/main/src/renderer/src/assets/images/shakespeareProud.webp'
   }
-  if (score<8) {
-    img = "https://raw.githubusercontent.com/Nic0Fernandez/Opti-Duolingo/main/src/renderer/src/assets/images/shakespeareProud.webp"
+  if (props.score < 8) {
+    imgScore.value =
+      'https://raw.githubusercontent.com/Nic0Fernandez/Opti-Duolingo/main/src/renderer/src/assets/images/shakespeareProud.webp'
   }
-  if (score<11) {
-    img = "https://raw.githubusercontent.com/Nic0Fernandez/Opti-Duolingo/main/src/renderer/src/assets/images/shakespeareProud.webp"
+  if (props.score < 11) {
+    imgScore.value =
+      'https://raw.githubusercontent.com/Nic0Fernandez/Opti-Duolingo/main/src/renderer/src/assets/images/shakespeareProud.webp'
   }
 }
 </script>
 
 <template>
   <div class="result-page">
-    <img :src="img" alt="Résultat de votre test" />
+    <img :src="imgScore" alt="Résultat de votre test" />
     <div class="score">Votre score est de : {{ score }}/10</div>
     <button class="buttonResult" @click="returnToHomePage">Retourner à l'accueil</button>
   </div>
